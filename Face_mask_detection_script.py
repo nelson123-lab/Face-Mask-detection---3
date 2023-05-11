37fbf75d115d09879376a0e3a9bdb468502c6aba
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-05-10 22:01:42 -0500

    Update Face_mask_detection_script.py

--- Face_mask_detection_script.py
+++ Face_mask_detection_script.py
@@ -1,1 +1,85 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import AveragePooling2D, Dropout, Flatten, Dense, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import os
+
+# Construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True, help="path to input dataset")
+ap.add_argument("-p", "--plot", type=str, default="plot.png", help="path to output loss/accuracy plot")
+ap.add_argument("-m", "--model", type=str, default="mask_detector.model", help="path to output face mask detector model")
+args = vars(ap.parse_args())
+
+# Initialize the initial learning rate, number of epochs, and batch size
+INIT_LR = 1e-4
+EPOCHS = 20
+BS = 32
+
+# Load and preprocess the images
+imagePaths = list(paths.list_images(args["dataset"]))
+data = []
+labels = []
+
+for imagePath in imagePaths:
+    label = imagePath.split(os.path.sep)[-2]
+    image = tf.keras.preprocessing.image.load_img(imagePath, target_size=(224, 224))
+    image = tf.keras.preprocessing.image.img_to_array(image)
+    image = tf.keras.applications.mobilenet_v2.preprocess_input(image)
+
+    data.append(image)
+    labels.append(label)
+
+# Convert the data and labels to NumPy arrays
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+# Perform one-hot encoding on the labels
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = tf.keras.utils.to_categorical(labels)
+
+# Split the data into training and testing sets
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.20, stratify=labels, random_state=42)
+
+# Construct the training image generator for data augmentation
+aug = ImageDataGenerator(
+    rotation_range=20,
+    zoom_range=0.15,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.15,
+    horizontal_flip=True,
+    fill_mode="nearest"
+)
+
+# Load the MobileNetV2 network, ensuring the head FC layer sets are left off
+baseModel = MobileNetV2(weights="imagenet", include_top=False, input_tensor=Input(shape=(224, 224, 3)))
+
+# Construct the head of the model that will be placed on top of the base model
+headModel = baseModel.output
+headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(128, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+headModel = Dense(2, activation="softmax")(headModel)
+
+# Place the head FC model on top of the base model (this will become the actual model we will train)
+model = Model(inputs=baseModel.input, outputs=headModel)
+
+# Loop over all layers in the base model and freeze them so they will not be updated during the first training process
+for layer in baseModel.layers:
+    layer.trainable = False
+
+# Compile the model
+opt = Adam(lr=INIT_LR
 
